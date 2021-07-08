b5e537cfdf68a058f890c7754f4e55bf3f0a6a8d
Trịnh Xuân Đạt <84932547+datIT-2000@users.noreply.github.com>
2021-07-08 11:57:35 +0700

    Create 1A.cpp

---
+++ lean/1000/1A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define ll int64_t
+#define ff first
+#define ss second
+#define pb(a) push_back(a)
+#define all(a) a.begin(),a.end()
+using namespace std;
+
+constexpr int mod = 1e9+7;
+
+int main()
+{
+	ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	cin.exceptions(cin.failbit);
+	srand(time(NULL));
+	ll n,m,a;
+	cin >> n >> m >> a;
+	cout << (n/a + (n%a!=0))*(m/a + (m%a!=0));
+	return 0;
+}
+
+
+
